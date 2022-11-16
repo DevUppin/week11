33b42a549a096824a68799e51ab69eb0889d60fb
Dev Uppin <devuppin@gmail.com>
2022-11-16 13:20:28 -0800

    done new activity

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,8 +3,23 @@
 //  }
 
 //  greet(Dev)
-const greet = function (user) {
-    console.log("Hello" + user)
+// const greet = function (user) {
+//     console.log("Hello" + user)
+//  }
+
+//  greet("Dev")
+
+const greet = function (user, time = "day") {
+    console.log(`Good ${time} ${user}`);
+
  }
 
- greet("Dev")
+ greet("dev", "night")
+
+ let radius = prompt("Enter a value: ")
+ const areaCircle = function (radius) {
+    if (radius > 0) {
+        alert(`${radius} * ${radius} * ${Math.PI} `)
+    }
+ }
+ areaCircle(radius)
