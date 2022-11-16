e85c5239623325b054561754557a8967d96db57c
Dev Uppin <devuppin@gmail.com>
2022-11-16 14:20:55 -0800

    done

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,17 +9,47 @@
 
 //  greet("Dev")
 
-const greet = function (user, time = "day") {
-    console.log(`Good ${time} ${user}`);
+// const greet = function (user, time = "day") {
+//     console.log(`Good ${time} ${user}`);
 
- }
+//  }
 
- greet("dev", "night")
+//  greet("dev", "night")
 
- let radius = prompt("Enter a value: ")
- const areaCircle = function (radius) {
-    if (radius > 0) {
-        alert(`${radius} * ${radius} * ${Math.PI} `)
+//  let radius = prompt("Enter a value: ")
+//  const areaCircle = function (radius) {
+//     let result = Math.PI * radius * radius;
+//     return result;
+// }
+// let r = prompt("Give a radius");
+// let calculatedArea = areaCircle(r).toFixed(2);
+
+// }document.getElementById(#radius)
+let shoppingItems = ["cheese", "bread", "green pepper"];
+const shoppingListElement = document.querySelector(".shopping")
+function populatList(arr) {
+    for (let item of arr) {
+        let listItem = document.createElement("li")
+        listItem.textContent = item;
+        shoppingListElement.appendChild(listItem)
+    } 
+}
+
+function squareToCircle() {
+    shoppingListElement.classList.remove("circleList");
+    const allListItems = shoppingListElement.classList.add("squareList")
+
+}
+function makeGreen() {
+    const allListItems = document.querySelectorAll(".shopping li");
+    for (let i = 0; i < allListItems.length; i++) {
+        if (allListItems.textContent.include("green")) {
+            allListItems[1].classList.add("green");
+        
+        }
     }
- }
- areaCircle(radius)
+
+}
+
+populatList(shoppingItems);
+squareToCircle()
